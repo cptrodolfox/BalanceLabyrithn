ab173131d73bd90c2eb0995b90f68933b6f60c44
William Arellano <william.arellano@yachaytech.edu.ec>
2018-05-04 07:07:21 -0500

    First commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    private Rigidbody rb;
+    private int score;
+    private AudioSource source;
+    public AudioClip pickSound;
+    public AudioClip winSound;
+	// Use this for initialization
+    void Awake (){
+        source = GetComponent<AudioSource>();
+    }
+	void Start () {
+        score = 0;
+        rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (score == 30)
+        {
+            source.PlayOneShot(winSound);
+            restartGame();
+        }
+	}
+    void restartGame(){
+
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    void OnCollisionEnter( Collision col )
+    {
+        if (col.gameObject.CompareTag("Collectable"))
+        {
+            col.gameObject.SetActive(false);
+            score = score + 1;
+            transform.localScale += new Vector3(0.1f,0.1f,0.1f);
+            rb.mass += 0.001f;
+            source.PlayOneShot(pickSound);
+        }
+    }
+}
---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    private AudioSource source;
+    public AudioClip loose;
+    public Transform pickUp;
+	// Use this for initialization
+    void Awake () {
+        source = GetComponent<AudioSource>();
+    }
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        for(int i=0; i < 30; i++){
+            Vector3 floorPosition = transform.position;
+            Vector3 position = new Vector3(Random.Range(-20.0f,20.0f), 5.0f, Random.Range(-20.0f,20.0f));
+            Instantiate(pickUp, position+floorPosition, Quaternion.identity);
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+        if (Input.GetKeyDown(KeyCode.R))
+            restartGame();
+        if (transform.position.y < 0.0f){
+            source.PlayOneShot(loose);
+            restartGame();
+
+        }
+	}
+
+    void FixedUpdate()
+    {
+        float moveZ = Input.GetAxis("Horizontal");
+        float moveX = Input.GetAxis("Vertical");
+        Vector3 rota = new Vector3 (moveX, 0.0f, moveZ);
+        Quaternion deltaRot = Quaternion.Euler(rota * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRot);
+    }
+
+    void restartGame ()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
